8dcd14019c9c6356c18f6e53d4751d440cd63b02
beauthebeau <beauthebeau@proton.me>
2024-02-09 17:37:18 +0000

    feat: add main.rs file to handle core logic
    Created `main` function with `shuttle_runtime` and `shuttle_secrets` attributes added.
    
     This function sets up the `poise` bot framework with an empty command vector, retrieves the `DISCORD_TOKEN` from `Secrets.toml` using the provided `SecretStore`, and builds a Client.

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use anyhow::Context as _;
+use poise::serenity_prelude::{ClientBuilder, GatewayIntents};
+use shuttle_secrets::SecretStore;
+use shuttle_serenity::ShuttleSerenity;
+
+struct Data {}
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+#[shuttle_runtime::main]
+async fn main(#[shuttle_secrets::Secrets] secret_store: SecretStore) -> ShuttleSerenity {
+
+    // Get DISCORD_TOKEN from `Secrets.toml`
+    let discord_token = secret_store
+        .get("DISCORD_TOKEN")
+        .context("'DISCORD_TOKEN' was not found")?;
+
+    let command_vec = vec![
+
+    ];
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: command_vec,
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = ClientBuilder::new(discord_token, GatewayIntents::non_privileged())
+        .framework(framework)
+        .await
+        .map_err(shuttle_runtime::CustomError::new)?;
+
+    Ok(client.into())
+}
